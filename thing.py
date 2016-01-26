aeef3f29ecc1592d5a1bccfec5cbe996e1fde8d4
ItsLukeJames <me@lukej.me>
2016-01-26 14:34:27 +0000

    wow
    first commit--ish

---
+++ thing.py
@@ -0,0 +1,51 @@
+#                                              
+#                                              
+#           .        .--.   _..._              
+#         .'|        |__| .'     '.   .--./)   
+#     .| <  |        .--..   .-.   . /.''\\    
+#   .' |_ | |        |  ||  '   '  || |  | |   
+# .'     || | .'''-. |  ||  |   |  | \`-' /    
+#'--.  .-'| |/.'''. \|  ||  |   |  | /("'`     
+#   |  |  |  /    | ||  ||  |   |  | \ '---.   
+#   |  |  | |     | ||__||  |   |  |  /'""'.\  
+#   |  '.'| |     | |    |  |   |  | ||     || 
+#   |   / | '.    | '.   |  |   |  | \'. __//  
+#   `'-'  '---'   '---'  '--'   '--'  `'---' 
+#"thing" database - github/ItsLukeJames
+
+#Imports
+import base64
+from Crypto.Cipher import AES
+import hashlib
+BLOCK_SIZE = 32
+PADDING = '{'
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+
+def encrypt(string, secret):
+    encoded = EncodeAES(AES.new(secret), string)
+    return encoded
+
+def decrypt(string, secret):
+    decoded = DecodeAES(AES.new(secret),string)
+    return decoded
+
+
+
+def start(file="db.thing", secret="gkgkjlekgjlrkejglkrfb,mfdnemfn,d"):
+    f = open(file, "r")
+    encrypted = f.read()
+    unencrypted = decrypt(encrypted, secret)
+    return eval(unencrypted)
+    
+def save(dict, file="db.thing", secret="gkgkjlekgjlrkejglkrfb,mfdnemfn,d"):
+    string = str(dict)
+    encrypted = encrypt(string, secret)
+    f = open(file, "w")
+    f.write(encrypted)
+    f.close()
+    return dict
+    
+    
+    
