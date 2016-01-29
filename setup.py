a56dfae3c508f7c560314bf875424428153524f4
ItsLukeJames <me@lukej.me>
2016-01-29 22:38:47 +0000

    this is so glitchy

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='thingdb',
-      version='0.2.2',
+      version='0.2.3',
       description="Python database using dict's",
       url='https://github.com/ItsLukeJames/thing',
       author='Luke James',
