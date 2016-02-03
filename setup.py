7ea5b950b3c3da615b684cbf168d7409b530d157
itslukej <me@lukej.me>
2016-02-03 21:53:07 +0000

    Username change

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='thingdb',
-      version='0.2.3',
+      version='0.2.4',
       description="Python database using dict's",
-      url='https://github.com/ItsLukeJames/thing',
+      url='https://github.com/itslukej/thing',
       author='Luke James',
       author_email='me@lukej.me',
       license='GNU',
