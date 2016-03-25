a8fde271a71028e6c39e68a31770642ca9c2ce34
itslukej <me@lukej.me>
2016-03-25 12:20:47 +0000

    This is thing 2.0 now

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='thingdb',
-      version='1.0',
+      version='2.0',
       description="Python database using dict's",
       url='https://github.com/itslukej/thing',
       author='Luke James',
