418b8b57656dc140ed38906f055c3edf50fa832c
itslukej <me@lukej.me>
2016-03-25 12:17:46 +0000

    Start thingdb2

--- thingdb/__init__.py
+++ thingdb/__init__.py
@@ -14,34 +14,49 @@
 #"thing" database - github/itslukej
 
 #Imports
-import base64
+import base64, hashlib, ast, os
 from Crypto.Cipher import AES
-import hashlib
-import ast
-BLOCK_SIZE = 32
-PADDING = '{'
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+class crypt(object):
+    def __init__(self):
+        self.BLOCK_SIZE = 32
+        self.PADDING = '{'
+        self.pad = lambda s: s + (self.BLOCK_SIZE - len(s) % self.BLOCK_SIZE) * self.PADDING
+        self.EncodeAES = lambda c, s: base64.b64encode(c.encrypt(self.pad(s)))
+        self.DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(self.PADDING)
+    def encrypt(self, string, secret):
+        secret = hashlib.sha224(secret).hexdigest()[:32]
+        encoded = self.EncodeAES(AES.new(secret), string)
+        return encoded
+    
+    def decrypt(self, string, secret):
+        secret = hashlib.sha224(secret).hexdigest()[:32]
+        decoded = self.DecodeAES(AES.new(secret),string)
+        return decoded
 
-def encrypt(string, secret):
-    encoded = EncodeAES(AES.new(secret), string)
-    return encoded
-
-def decrypt(string, secret):
-    decoded = DecodeAES(AES.new(secret),string)
-    return decoded
-
-def start(filename="db.thing", secret="gkgkjlekgjlrkejglkrfb,mfdnemfn,d"):
-    f = open(filename, "r")
-    encrypted = f.read()
-    unencrypted = decrypt(encrypted, secret)
-    return ast.literal_eval(unencrypted)
-
-def save(to_save, filename="db.thing", secret="gkgkjlekgjlrkejglkrfb,mfdnemfn,d"):
-    string = str(to_save)
-    encrypted = encrypt(string, secret)
-    f = open(filename, "w")
-    f.write(encrypted)
-    f.close()
-    return to_save
+class thing(object):
+    def __init__(self, filename="db.thing", secret="password"):
+        #specify things
+        self.crypt_wrapper = crypt()
+        self.filename = filename
+        self.secret = secret
+        #
+        if not os.path.isfile(self.filename):
+            f = open(self.filename, "w+")
+            f.write(self.crypt_wrapper.encrypt("{}", self.secret))
+            f.close()
+        f = open(filename, "r")
+        unencrypted = self.crypt_wrapper.decrypt(f.read(), secret)
+        self.dict = ast.literal_eval(unencrypted)
+    def save(self):
+        string = str(self.dict)
+        encrypted = self.crypt_wrapper.encrypt(string, self.secret)
+        f = open(self.filename, "w")
+        f.write(encrypted)
+        f.close()
+        return self.dict
+    #other stuff
+    def keys(self):
+        return self.dict.keys()
+        
+    def has_key(self, key):
+        return key in self.dict
