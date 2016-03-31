64f4422f29354b6e29e7672c0a0c1175b634a77e
itslukej <me@lukej.me>
2016-03-31 23:59:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='thingdb',
-      version='2.2.2',
+      version='2.2.3',
       description="Python database using dict's",
       url='https://github.com/itslukej/thing',
       author='Luke James',
