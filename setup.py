3b929a7b56abf301637c94191a1741ba1e3d605b
itslukej <me@lukej.me>
2016-05-05 20:17:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='thingdb',
-      version='2.2.3',
+      version='3.0.0',
       description="Python database using dict's",
       url='https://github.com/itslukej/thing',
       author='Luke James',
       author_email='me@lukej.me',
       license='GNU',
       packages=find_packages(),
-      install_requires=['pycrypto'],
       zip_safe=False)
