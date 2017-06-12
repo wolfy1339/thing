14402872bfe1dcacb6c897c24cf5a8e8d2f5e523
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-06-12 11:49:15 -0400

    Code formatting improvements

--- thingdb/__init__.py
+++ thingdb/__init__.py
@@ -21,23 +21,26 @@ try:
 except ImportError:
     import dbm as anydbm
 
+
 class thing(Wrapper):
     def __init__(self, filename, flag="c"):
-        database = anydbm.open(filename, flag)
-        self.filename=filename
+        super(thing, self).__init__(anydbm.open(filename, flag))
+        self.filename = filename
         self.flags = flag
-        Wrapper.__init__(self, database)
+
     def sync(self):
         if type(self.dict) is not ClosedDict:
             for key, entry in self.cache.items():
                 self.dict[key] = encoder.enpickle(entry)
             self.cache = {}
-            #if hasattr(self.dict, 'save'):
+            # if hasattr(self.dict, 'save'):
             #    self.dict.save()
+
     def close(self):
         if type(self.dict) is not ClosedDict:
             self.sync()
             self.dict.close()
             self.dict = ClosedDict()
+
     def __repr__(self):
         return "thingdb({0})".format(self.filename)
