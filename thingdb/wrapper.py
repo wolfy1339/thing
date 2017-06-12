63a33f4a08e56bacbd258156deb17dd0d43e4438
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-06-12 11:49:35 -0400

    Code formatting improvements

--- thingdb/wrapper.py
+++ thingdb/wrapper.py
@@ -5,35 +5,40 @@ except ImportError:
 
 from . import encoder
 
+
 class Wrapper(DictMixin):
     """Main wrapper for thingdb"""
-    def __init__(self, dict):
-        self.dict = dict
-        self.cache={}
+    def __init__(self, database):
+        super(Wrapper, self).__init__()
+        self.dict = database
+        self.cache = {}
+
     def keys(self):
-        _keys = []
-        for x in self.dict.keys():
-            _keys.append(x)
-        for x in self.cache.keys():
-            _keys.append(x)
-        return _keys
+        return list(self.dict.keys()).extend(list(self.cache.keys()))
+
     def has_key(self, key):
         return key in self.dict
+
     def __getitem__(self, key):
         if key not in self.cache.keys():
             self.cache[key] = encoder.depickle(self.dict[key])
         return self.cache[key]
+
     def __setitem__(self, key, value):
         self.cache[key] = value
+
     def __delitem__(self, key):
         try:
             del self.dict[key]
         except KeyError:
             del self.cache[key]
+
     def __contains__(self, key):
         return key in self.dict
+
     def __len__(self):
         return len(self.dict)
+
     def __iter__(self):
         for k in self.dict.keys():
             if k in self.cache.keys:
